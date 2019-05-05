2a86393dc63d2042e50c4636941616d0d1107280
saeedPadyab <padyabsaeed@gmail.com>
2019-05-05 17:26:08 +0300

    side menu names

--- src/Packages/Home/components/SideBar/components/SideLinks/index.jsx
+++ src/Packages/Home/components/SideBar/components/SideLinks/index.jsx
@@ -8,48 +8,37 @@ const NavLinks = props => {
   }
   const links = [
     {
-      name: translate("HOME_SIDE_NAV_CONTENT_TYPE"),
+      name: translate("Products"),
       icon: "item-type",
       path: "/panel/contentType",
       desc: translate("HOME_SIDE_NAV_CONTENT_TYPE_DEC")
     },
     {
-      name: translate("HOME_SIDE_NAV_CATEGRIES"),
+      name: translate("Requests"),
       icon: "category",
       path: "/panel/categories",
       desc: translate("HOME_SIDE_NAV_CATEGORIES_DEC")
     },
     {
-      name: translate("HOME_SIDE_NAV_PRODUCTS"),
+      name: translate("Quotes"),
       icon: "product",
       path: "/panel/contents",
       desc: translate("HOME_SIDE_NAV_PRODUCTS_DESC")
     },
 
-    // {
-    //   name: translate("HOME_SIDE_NAV_REQUESTS"),
-    //   icon: "request",
-    //   path: "/home/requests",
-    //   desc: translate("HOME_SIDE_NAV_REQUESTS_DESC")
-    // },
-    // {
-    //   name: translate("HOME_SIDE_NAV_QUOTES"),
-    //   icon: "quote",
-    //   path: "/home/quotes",
-    //   desc: translate("HOME_SIDE_NAV_QUOTES_DESC")
-    // },
-    // {
-    //   name: translate("HOME_SIDE_NAV_MANAGET_USERS"),
-    //   icon: "users",
-    //   path: "/panel/users",
-    //   desc: translate("HOME_SIDE_NAV_MANAGE_USERS_DESC")
-    // },
     {
-      name: translate("HOME_SIDE_NAV_ASSETS_MANAGER"),
-      icon: "images",
-      path: "/panel/assets",
-      desc: translate("HOME_SIDE_NAV_ASSETS_MANAGER_DESC")
-    }
+      name: translate("Users"),
+      icon: "request",
+      path: "/home/requests",
+      desc: translate("HOME_SIDE_NAV_REQUESTS_DESC")
+    },
+    {
+      name: translate("Marketing"),
+      icon: "quote",
+      path: "/home/quotes",
+      desc: translate("HOME_SIDE_NAV_QUOTES_DESC")
+    },
+  
   ];
 
   return (
